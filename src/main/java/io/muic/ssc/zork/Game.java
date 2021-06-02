aea3c84e0aaee5ff98b53470fe17012241bc8a6e
Vanessa5331 <earth5331@gmail.com>
2021-06-02 22:17:47 +0700

    Create Main class and Game class

---
+++ src/main/java/io/muic/ssc/zork/Game.java
@@ -0,0 +1,11 @@
+package io.muic.ssc.zork;
+
+import java.util.Scanner;
+
+public class Game {
+    public void run() {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println(s);
+    }
+}
